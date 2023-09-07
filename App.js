66122cf305344a60bf8b8209396dfff8c6c12b29
furqan <furqanali945@gmail.com>
2023-09-07 22:29:40 +0500

    Added parcel in the project using NPM also added git ignore file for node modules.

---
+++ App.js
@@ -0,0 +1,14 @@
+const parent = React.createElement('div',{className: "parent"},[
+    React.createElement('div', {className: "child"} , [
+        React.createElement('h1', {} , "First Child Headign 1"),
+        React.createElement('h2', {} , "First Child Headign 2"),
+    ]),
+    React.createElement('div', {className: "child"} , [
+        React.createElement('h1', {} , "First Child Headign 1"),
+        React.createElement('h2', {} , "First Child Headign 2"),
+    ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
