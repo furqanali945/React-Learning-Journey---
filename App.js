85cfc43673251b97abf788cc32fe1037c373507e
furqan <furqanali945@gmail.com>
2023-09-12 03:21:07 +0500

    Getting to know jsx and components in React. Also, learned about react components!

--- App.js
+++ App.js
@@ -8,8 +8,31 @@ const jsxHeading = <h1 className="heading" tabIndex="1">Just for testing JSX!</h
 const multipleHeading = (<h1 className="heading" tabIndex="1">Just for testing JSX!
 </h1>);
 
-
 console.log(jsxHeading);
 
+// React Component
+// Class based components - old way 
+// Functional components -  new way 
+
+
+// react element
+const heading = (<h1 className="heading" tabIndex="1">Just for testing JSX!
+</h1>);
+
+const Title = () => (
+    <h1 className="heading" tabIndex="1">This is a Heading Component!</h1>
+);
+
+// the other arrow function way | react functional component | We'll stick to this syntax
+// this is also an example of component composition
+const Header = () => (
+    <div className="container"> 
+        <Title />
+        <h2 className="heading">Namaste React Functional Component!</h2>
+        <h3>{1+2}</h3>
+    </div>  
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+
+root.render(<Header />);
