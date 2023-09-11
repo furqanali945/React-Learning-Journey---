717e590d4897a45920f4db2d22d652791a0d8db6
furqan <furqanali945@gmail.com>
2023-09-11 05:22:32 +0500

    Starting with JSX!

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1>Just for testing JSX!</h1>;
+// transpilled before reaching the javascript engine - Parcel with the help of babel
+const jsxHeading = <h1 className="heading" tabIndex="1">Just for testing JSX!</h1>;
+
+// you need to wrap it in round bracket for multiple lines!
+const multipleHeading = (<h1 className="heading" tabIndex="1">Just for testing JSX!
+</h1>);
+
 
 console.log(jsxHeading);
 
