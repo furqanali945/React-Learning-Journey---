59a8468e27a9aeafb0ccea812b7a567d9c0877b6
furqan <furqanali945@gmail.com>
2023-09-18 04:59:13 +0500

    Looking into existing code and checking different concepts.

--- App.js
+++ App.js
@@ -1886,6 +1886,13 @@ const RestaurantCard = ({
 
 // Body Component for body section: It contain all restaurant cards
 // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
+// why we need unique id as key in looping elements?
+// suppose there are a new element added so react will render all of the cards
+// so react will not know where they need to add a new card
+// but with id it will know exactly where to place the new card.
+// it can affect the speed if you don't add the key so always add key on looping items.
+// you should never use index as key as in map you will also get index it is marked as bad practice in react documentation.
+
 
 const Body = () => {
     return (
