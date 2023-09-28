1ebba93ccae7d682cd5004d199e23142454c9b31
furqan <furqanali945@gmail.com>
2023-09-28 21:05:08 +0500

    Did some styling related changes to shimmer card css.

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,13 +2,16 @@ const Shimmer = () => {
     return (
         <div className="shimmer_container">
             <div className="shimmer_card">
-                Card
+               
             </div>
             <div className="shimmer_card">
-                Card
+               
             </div>
             <div className="shimmer_card">
-                Card
+               
+            </div>
+            <div className="shimmer_card">
+               
             </div>
         </div>
     )
