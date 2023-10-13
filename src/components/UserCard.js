d0d955d1b5be998b0d696895808906c50d49260b
furqan <furqanali945@gmail.com>
2023-10-14 02:49:37 +0500

    Looking into using state variables in classed based component as compare to functional components.

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 const UserCard = ({name}) => {
+
+    const [count] = useState(0);    
+    const [count_2] = useState(1);   
+
     return(
         <div className="usercard">
             <ul>
                 <li>
-                    <strong>Name:</strong> {name}
+                    <strong>Name:</strong> {name} - {count} - {count_2}
                 </li>
                 <li>
                    <strong>Github:</strong> @furqanali945
--- src/components/UserCardClass.js
+++ src/components/UserCardClass.js
@@ -3,16 +3,23 @@ import React from "react";
 class UserCardClass extends React.Component{
     constructor(props){
         super(props); // Call the constructor of the parent class (React.Component)
+        
+        this.state = {
+            count: 0,
+            count_2: 1,
+        };
+    
     }
-    }
+
     render(){
         const {name} = this.props;
+        const {count,count_2} = this.state;
 
         return(
             <div className="usercard">
                 <ul>
                     <li>
-                        <strong>Name:</strong> {name}
+                        <strong>Name:</strong> {name} - {count} - {count_2}
                     </li>
                     <li>
                        <strong>Github:</strong> @furqanali945
