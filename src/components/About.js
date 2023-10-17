8c9dd1262ded74f594b8c01afaa7520224110978
furqan <furqanali945@gmail.com>
2023-10-18 03:21:14 +0500

    Completed the API call also look into react render lifecycle about mount, update and unmount!.

--- src/components/About.js
+++ src/components/About.js
@@ -5,24 +5,40 @@ class About extends React.Component{
     constructor(props){
         super(props);
         // console.log("Parent constructor called.");
+
+        this.state = {
+            userInfo:{}
+        }
     }
 
     async componentDidMount(){
         // console.log("Parent did mount.");
         // API call
-        const data =  await fetch("https://api.github.com/users/akshaymarch7");
+        const data =  await fetch("https://api.github.com/users/furqanali945");
         const json =  await data.json();
+
+        this.setState({
+            userInfo: json
+        })    
+
         console.log(json);
+    }
+
+    componentDidUpdate(){
+        console.log("Component Did update called!");
     }
 
     render(){
         // console.log("Parent render!");
+
+        const {name,location,avatar_url} = this.state.userInfo;
+
         return (
             <div className="about_page">
                 <div className="container">
                     <h1>About Us Page</h1>
                     <h2>Team Members</h2>
-                    <UserCardClass name="Syed Furqan Ali" />
+                    <UserCardClass name={name} location={location} avatar_url={avatar_url}/>
                 </div>
             </div>
         )
--- src/components/UserCardClass.js
+++ src/components/UserCardClass.js
@@ -3,7 +3,6 @@ import React from "react";
 class UserCardClass extends React.Component{
     constructor(props){
         super(props); // Call the constructor of the parent class (React.Component)
-
         // console.log("Child " + this.props.name + " constructor.");
     }
 
@@ -13,13 +12,15 @@ class UserCardClass extends React.Component{
 
     render(){
         
-        const {name} = this.props;
-
-        console.log("Child "+ name +" render.");
+        const {name,location, avatar_url} = this.props;
+        //console.log("Child "+ name +" render.");
 
         return(
             <div className="usercard">
-                <ul>
+                <ul>    
+                    <li>
+                        <img src={avatar_url}></img>
+                    </li>
                     <li>
                         <strong>Name:</strong> {name}
                     </li>
@@ -27,6 +28,7 @@ class UserCardClass extends React.Component{
                        <strong>Github:</strong> @furqanali945
                     </li>
                     <li><strong>Designation:</strong> Senior Web Developer</li>
+                    <li><strong>Location:</strong> {location}</li>
                 </ul>
             </div>
         )
